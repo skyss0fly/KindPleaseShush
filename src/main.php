642c9ce8da818ad21997fb2ef2023d5472a85734
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-05-14 11:47:24 +1000

    Save main.php.

--- src/main.php
+++ src/main.php
@@ -3,13 +3,19 @@ namespace skyss0fly\KindPleaseShush;
 
 use pocketmine\Plugin\PluginBase;
 use pocketmine\utils\TextFormat;
-
+use pocketmine\player\Player;
+use skyss0fly\src\command\Command;
 Class Main extends PluginBase():{
     public function onLoad(): void{
         $this->getLogger()info->('Kindly Ask someone to shut up using' ..FORMAT:MINECOIN_GOLD.. '/kindshush')
-    
+    public function onEnable(): void{
+        self::$instance = $this;
+		$this->getServer()->getCommandMap()->register("kindshush", new Command());
+	}
+    }
     }
     public function usecommand(): void {
         $this->getServer()->broadcastMessage(..TEXTFORMAT:RED.. ..$playerusecommand.. 'Has Kindly asked you to shush @' ..$playertarget..)
     }
 }
+
