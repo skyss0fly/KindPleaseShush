54084ffdf29375ae24bf4d0f8cf926bc973b5220
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-07-08 19:38:08 +1000

    Update main.php

--- src/main.php
+++ src/main.php
@@ -8,16 +8,30 @@ use command\Command;
 
 Class Main extends PluginBase{
     
-    public function onLoad():: voic{
+    public function onLoad(): void{
         $this->getLogger()->info('Kindly Ask someone to shut up using /kindshush')
     }
-    public function onEnable():: void{
+    public function onEnable(): void{
         self::$instance = $this;
 		$this->getServer()->getCommandMap()->register("kindshush", new Command());
 	}
     
-    public function usecommand()::void {
-        $this->getServer()->broadcastMessage(..TEXTFORMAT:RED.. ..$playerusecommand.. 'Has Kindly asked you to shush @' ..$playertarget..)
-    }
+   public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
+        switch ($command->getName()) {
+            case "kindshush":
+       if (count($args) === 0) {
+                $sender->sendMessage("Usage: /kindshush <player>");
+                return true;
+            }
+
+            $targetPlayer = $this->getServer()->getPlayerExact($args[0]);
+            if (!$targetPlayer instanceof Player) {
+                $sender->sendMessage("Player not found.");
+                return true;
+            }
+		$this->getServer()->broadcastMessage($sender . "is kindly asking " . $targetPlayer . " to shut up. if you were wise, you would shush!");
+
 }
+   }
+    }
 
