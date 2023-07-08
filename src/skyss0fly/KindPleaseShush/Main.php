2a98a6a3be9a33b5cad5fcbb11ba17b3571707bf
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-07-08 19:47:19 +1000

    Update Main.php

--- src/skyss0fly/KindPleaseShush/Main.php
+++ src/skyss0fly/KindPleaseShush/Main.php
@@ -7,32 +7,30 @@ use pocketmine\player\Player;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 
-Class Main extends PluginBase{
+Class Main extends PluginBase { 
     
     public function onLoad(): void{
-        $this->getLogger()->info('Kindly Ask someone to shut up using /kindshush')
-    }
+        $this->getLogger()->info('Kindly Ask someone to shut up using /kindshush');
+		}
     public function onEnable(): void{
         self::$instance = $this;
 		$this->getServer()->getCommandMap()->register("kindshush", new Command());
+	
 	}
-    
    public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
         switch ($command->getName()) {
             case "kindshush":
        if (count($args) === 0) {
                 $sender->sendMessage("Usage: /kindshush <player>");
                 return true;
-            }
+       }
 
             $targetPlayer = $this->getServer()->getPlayerExact($args[0]);
             if (!$targetPlayer instanceof Player) {
                 $sender->sendMessage("Player not found.");
                 return true;
-            }
-		$this->getServer()->broadcastMessage($sender . "is kindly asking " . $targetPlayer . " to shut up. if you were wise, you would shush!");
-
+	    }
+	$this->getServer()->broadcastMessage($sender . "is kindly asking " . $targetPlayer . " to shut up. if you were wise, you would shush!");
+	}}
 }
-   }
-    }
-
+    
